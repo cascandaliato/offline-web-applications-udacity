c0e0bc817c9de8f0db48f39d3cfa80c6118e044f
cascandaliato <8927157+cascandaliato@users.noreply.github.com>
2020-10-21 19:29:29 +0200

    cache photos and avatars

--- wittr/public/js/main/IndexController.js
+++ wittr/public/js/main/IndexController.js
@@ -1,15 +1,15 @@
-import PostsView from './views/Posts';
-import ToastsView from './views/Toasts';
-import idb from 'idb';
+import PostsView from "./views/Posts";
+import ToastsView from "./views/Toasts";
+import idb from "idb";
 
 function openDatabase() {
   if (!navigator.serviceWorker) {
     return Promise.resolve();
   }
 
-  return idb.open('wittr-db', 1, function (upgradeDb) {
-    const store = upgradeDb.createObjectStore('wittrs', { keyPath: 'id' });
-    store.createIndex('by-date', 'time');
+  return idb.open("wittr-db", 1, function (upgradeDb) {
+    const store = upgradeDb.createObjectStore("wittrs", { keyPath: "id" });
+    store.createIndex("by-date", "time");
     // switch (upgradeDb.oldVersion) {
     //   case 0:
     //     const wittrsStore = upgradeDb.createObjectStore('wittrs', { keyPath: 'id' });
@@ -26,8 +26,13 @@ export default function IndexController(container) {
   // this._openSocket();
   // this._dbPromise = openDatabase();
   this._registerServiceWorker();
+  this._cleanImageCache();
 
   const indexController = this;
+
+  setInterval(function () {
+    indexController._cleanImageCache();
+  }, 1000 * 60 * 5);
 
   this._showCachedMessages().then(function () {
     indexController._openSocket();
@@ -41,7 +46,7 @@ IndexController.prototype._registerServiceWorker = function () {
 
   // navigator.serviceWorker.register('/sw.js', { scope: '/' });
   navigator.serviceWorker
-    .register('/sw.js')
+    .register("/sw.js")
     .then(function (reg) {
       if (!navigator.serviceWorker.controller) return;
 
@@ -55,37 +60,37 @@ IndexController.prototype._registerServiceWorker = function () {
         return;
       }
 
-      reg.addEventListener('updatefound', function () {
+      reg.addEventListener("updatefound", function () {
         indexController._trackInstalling(reg.installing);
       });
-      console.log('Registration worked!');
+      console.log("Registration worked!");
     })
     .catch(function () {
-      console.log('Registration failed!!');
+      console.log("Registration failed!!");
     });
 
-  navigator.serviceWorker.addEventListener('controllerchange', function () {
+  navigator.serviceWorker.addEventListener("controllerchange", function () {
     window.location.reload();
   });
 };
 
 IndexController.prototype._trackInstalling = function (worker) {
   var indexController = this;
-  worker.addEventListener('statechange', function () {
-    if (worker.state == 'installed') {
+  worker.addEventListener("statechange", function () {
+    if (worker.state == "installed") {
       indexController._updateReady(worker);
     }
   });
 };
 
 IndexController.prototype._updateReady = function (worker) {
-  var toast = this._toastsView.show('New version available', {
-    buttons: ['refresh', 'dismiss'],
+  var toast = this._toastsView.show("New version available", {
+    buttons: ["refresh", "dismiss"],
   });
 
   toast.answer.then(function (answer) {
-    if (answer != 'refresh') return;
-    worker.postMessage({ action: 'skipWaiting' });
+    if (answer != "refresh") return;
+    worker.postMessage({ action: "skipWaiting" });
   });
 };
 
@@ -95,37 +100,37 @@ IndexController.prototype._openSocket = function () {
   var latestPostDate = this._postsView.getLatestPostDate();
 
   // create a url pointing to /updates with the ws protocol
-  var socketUrl = new URL('/updates', window.location);
-  socketUrl.protocol = 'ws';
+  var socketUrl = new URL("/updates", window.location);
+  socketUrl.protocol = "ws";
 
   if (latestPostDate) {
-    socketUrl.search = 'since=' + latestPostDate.valueOf();
+    socketUrl.search = "since=" + latestPostDate.valueOf();
   }
 
   // this is a little hack for the settings page's tests,
   // it isn't needed for Wittr
-  socketUrl.search += '&' + location.search.slice(1);
+  socketUrl.search += "&" + location.search.slice(1);
 
   var ws = new WebSocket(socketUrl.href);
 
   // add listeners
-  ws.addEventListener('open', function () {
+  ws.addEventListener("open", function () {
     if (indexController._lostConnectionToast) {
       indexController._lostConnectionToast.hide();
     }
   });
 
-  ws.addEventListener('message', function (event) {
+  ws.addEventListener("message", function (event) {
     requestAnimationFrame(function () {
       indexController._onSocketMessage(event.data);
     });
   });
 
-  ws.addEventListener('close', function () {
+  ws.addEventListener("close", function () {
     // tell the user
     if (!indexController._lostConnectionToast) {
       indexController._lostConnectionToast = indexController._toastsView.show(
-        'Unable to connect. Retrying…',
+        "Unable to connect. Retrying…"
       );
     }
 
@@ -136,6 +141,37 @@ IndexController.prototype._openSocket = function () {
   });
 };
 
+IndexController.prototype._cleanImageCache = function () {
+  return this._dbPromise.then(function (db) {
+    if (!db) return;
+
+    const imagesNeeded = [];
+    const tx = db.transaction("wittrs");
+    return tx
+      .objectStore("wittrs")
+      .getAll()
+      .then(function (messages) {
+        messages.forEach(function (message) {
+          if (message.photo) {
+            imagesNeeded.push(message.photo);
+          }
+          imagesNeeded.push(message.avatar);
+        });
+
+        return caches.open("wittr-content-imgs").then(function (cache) {
+          return cache.keys().then(function (requests) {
+            requests.forEach(function (request) {
+              const url = new URL(request.url);
+              if (!imagesNeeded.includes(url.pathname)) {
+                cache.delete(request);
+              }
+            });
+          });
+        });
+      });
+  });
+};
+
 // called when the web socket sends message data
 IndexController.prototype._onSocketMessage = function (data) {
   var messages = JSON.parse(data);
@@ -143,19 +179,23 @@ IndexController.prototype._onSocketMessage = function (data) {
   this._dbPromise.then(function (db) {
     if (!db) return;
 
-    const tx = db.transaction('wittrs', 'readwrite');
-    const store = tx.objectStore('wittrs');
+    const tx = db.transaction("wittrs", "readwrite");
+    const store = tx.objectStore("wittrs");
     messages.foEach(function (message) {
       store.put(message);
     });
 
-    return store.index('by-date').openCursor(null, 'prev').then(function (cursor) {
-      return cursor.advance(30);
-    }).then(function deleteRest(cursor) {
-      if (!cursor) return;
-      cursor.delete();
-      return cursor.continue().then(deleteRest);
-    });
+    return store
+      .index("by-date")
+      .openCursor(null, "prev")
+      .then(function (cursor) {
+        return cursor.advance(30);
+      })
+      .then(function deleteRest(cursor) {
+        if (!cursor) return;
+        cursor.delete();
+        return cursor.continue().then(deleteRest);
+      });
   });
 
   this._postsView.addPosts(messages);
@@ -167,11 +207,12 @@ IndexController.prototype._showCachedMessages = function () {
   return this._dbPromise.then(function (db) {
     if (!db || indexController._postsView.showingPosts()) return;
 
-    const index = db.transaction('wittrs')
-      .objectStore('wittrs')
-      .store.index('age');
+    const index = db
+      .transaction("wittrs")
+      .objectStore("wittrs")
+      .store.index("age");
     return index.getAll().then(function (messages) {
-     indexController._postsView.addPosts(messages.reverse());
+      indexController._postsView.addPosts(messages.reverse());
     });
   });
-}
+};
--- wittr/public/js/sw/index.js
+++ wittr/public/js/sw/index.js
@@ -1,18 +1,23 @@
-const staticCacheName = 'wittr-static-v6';
-self.addEventListener('install', (event) => {
+const staticCacheName = "wittr-static-v6";
+const contentImgsCache = "wittr-content-imgs";
+const allCaches = [staticCacheName, contentImgsCache];
+
+self.addEventListener("install", (event) => {
   const urlsToCache = [
-    '/skeleton',
-    'js/main.js',
-    'css/main.css',
-    'imgs/icon.png',
-    'https://fonts.gstatic.com/s/roboto/v15/2UX7WLTfW3W8TclTUvlFyQ.woff',
-    'https://fonts.gstatic.com/s/roboto/v15/d-6IYplOFocCacKzxwXSOD8E0i7KZn-EPnyo3HZu7kw.woff',
+    "/skeleton",
+    "js/main.js",
+    "css/main.css",
+    "imgs/icon.png",
+    "https://fonts.gstatic.com/s/roboto/v15/2UX7WLTfW3W8TclTUvlFyQ.woff",
+    "https://fonts.gstatic.com/s/roboto/v15/d-6IYplOFocCacKzxwXSOD8E0i7KZn-EPnyo3HZu7kw.woff",
   ];
 
-  event.waitUntil(caches.open(staticCacheName).then((cache) => cache.addAll(urlsToCache)));
+  event.waitUntil(
+    caches.open(staticCacheName).then((cache) => cache.addAll(urlsToCache))
+  );
 });
 
-self.addEventListener('activate', (event) => {
+self.addEventListener("activate", (event) => {
   // event.waitUntil(caches.delete('wittr-static-v1'));
 
   event.waitUntil(
@@ -21,14 +26,17 @@ self.addEventListener('activate', (event) => {
       .then((cacheNames) =>
         Promise.all(
           cacheNames
-            .filter((cacheName) => cacheName.startsWith('wittr-') && cacheName != staticCacheName)
-            .map((cacheName) => caches.delete(cacheName)),
-        ),
-      ),
+            .filter(
+              (cacheName) =>
+                cacheName.startsWith("wittr-") && !allCaches.includes(cacheName)
+            )
+            .map((cacheName) => caches.delete(cacheName))
+        )
+      )
   );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener("fetch", (event) => {
   // console.log('foo', event.request);
 
   // event.respondWith(
@@ -50,8 +58,16 @@ self.addEventListener('fetch', (event) => {
 
   var requestUrl = new URL(event.request.url);
   if (requestUrl.origin === location.origin) {
-    if (requestUrl.pathname === '/') {
-      event.respondWith(caches.match('/skeleton'));
+    if (requestUrl.pathname === "/") {
+      event.respondWith(caches.match("/skeleton"));
+      return;
+    }
+    if (requestUrl.pathname.startsWith("/photos/")) {
+      event.respondWith(servePhoto(event.request));
+      return;
+    }
+    if (requestUrl.pathname.startsWith("/avatars/")) {
+      event.respondWith(serveAvatar(event.request));
       return;
     }
   }
@@ -60,10 +76,36 @@ self.addEventListener('fetch', (event) => {
     caches
       .open(staticCacheName)
       .then((cache) => cache.match(event.request))
-      .then((response) => response || fetch(event.request.url)),
+      .then((response) => response || fetch(event.request.url))
   );
 });
 
-self.addEventListener('message', function (event) {
-  if (event.data.action === 'skipWaiting') self.skipWaiting();
+function servePhoto(request) {
+  const storageUrl = request.url.replace(/-\d+px\.jpg$/, "");
+  return caches.open(contentImgsCache).then((cache) => {
+    return cache.match(storageUrl).then((response) => {
+      if (response) return response;
+      return fetch(request.url).then((networkResponse) => {
+        cache.put(storageUrl, networkResponse.clone());
+        return networkResponse;
+      });
+    });
+  });
+}
+
+function serveAvatar(request) {
+  const storageUrl = request.url.replace(/-\dx\.jpg$/, "");
+  return caches.open(contentImgsCache).then((cache) => {
+    return cache.match(storageUrl).then((response) => {
+      const networkFetch = fetch(request.url).then((networkResponse) => {
+        cache.put(storageUrl, networkResponse.clone());
+        return networkResponse;
+      });
+      return response || networkFetch;
+    });
+  });
+}
+
+self.addEventListener("message", function (event) {
+  if (event.data.action === "skipWaiting") self.skipWaiting();
 });
